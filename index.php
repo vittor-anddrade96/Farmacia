ee7b96eedddfaad0e98df669465e2043034ac2f7
vittor-anddrade96 <vittor.anddrade@gmail.com>
2024-10-13 21:44:35 -0300

    .

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+    require 'conexao.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Farmácia Vida Saudável</title>
+</head>
+<body>
+    <header>
+        <div id="elemento-pai">
+            <a href="index.php"><img id="logo" src="imagens/farmacia.png" width="100"></a>
+        </div>
+    </header>
+</body>
+</html>
