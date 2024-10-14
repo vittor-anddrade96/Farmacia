654c6afd13848b92d0d22ddc4705c0a37c5e0ebe
vittor-anddrade96 <vittor.anddrade@gmail.com>
2024-10-13 22:28:21 -0300

    .

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
     require 'conexao.php';
+    $sql = $pdo->query("SELECT * FROM medicamentos");
+
+    $lista = [];
+    $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
@@ -9,6 +13,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet" />
     <title>Farmácia Vida Saudável</title>
 </head>
 <body>
@@ -16,6 +21,44 @@
         <div id="elemento-pai">
             <a href="index.php"><img id="logo" src="imagens/farmacia.png" width="100"></a>
         </div>
+        <div id="login">
+            <small><a href="login.php">Login</a></small>
+        </div>
     </header>
+    <nav>
+        <div class="menu">
+            <a href="cadMedicamento.php">Cadastro de Medicamentos</a>
+            <a href="editMedicamento.php">Editar Medicamentos</a>
+            <a href="excluirMedicamento.php">Ecluir Medicamentos</a>
+            <hr>
+        </div>
+    </nav>
+    <h3>MEDICAMENTOS EM ESTOQUE</h3>
+    <table border="1px">
+        <tr>
+            <th>ID</th>
+            <th>MEDICAMENTO</th>
+            <th>VALOR</th>
+            <th>ESTOQUE</th>
+            <th>CATEGORIA</th>
+            <th>VALIDADE</th>
+        </tr>
+        <?php foreach($lista as $a): ?>
+            <tr>
+                <td> <?php echo $a['id'];?> </td>
+                <td> <?php echo $a['medicamento'];?> </td>
+                <td> <?php echo $a['valor'];?> </td>
+                <td> <?php echo $a['estoque'];?> </td>
+                <td> <?php echo $a['categoria'];?> </td>
+                <td> <?php echo $a['validade'];?> </td>
+                <td>
+                    <a href="editMedicamento.php?id=<?=$a['id']; ?>">[Editar]</a>
+                    <a href="excluirMedicamento.php?id=<?=$a['Excluir']; ?>">[Excluir]</a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+    </table>
+    <br>
+    <a href="vender.php">INICIAR VENDA</a>
 </body>
 </html>
