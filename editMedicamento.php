0982cf1a57468d7bc906508cb71a1ba342a3c087
vittor-anddrade96 <vittor.anddrade@gmail.com>
2024-10-14 19:21:52 -0300

    Update editMedicamento.php

--- editMedicamento.php
+++ editMedicamento.php
@@ -9,13 +9,14 @@ if (isset($_POST['acao'])) {
     $categoria = $_POST['categoria'];
     $validade = $_POST['validade'];
 
-    $sql = $pdo->prepare("UPDATE medicamentos SET medicamento = :medicamento, valor = :valor, estoque = :estoque, categoria = :categoria, validade = :validade WHERE id = $id");
+    $sql = $pdo->prepare("UPDATE medicamentos SET medicamento = :medicamento, valor = :valor, estoque = :estoque, categoria = :categoria, validade = :validade WHERE id = :id");
     $sql->bindValue(':medicamento', $medicamento);
     $sql->bindValue(':valor', $valor);
     $sql->bindValue(':estoque', $estoque);
     $sql->bindValue(':categoria', $categoria);
     $sql->bindValue(':validade', $validade);
-
+    $sql->bindValue(':id', $id);
+    
     if ($sql->execute()) {
         echo 'Medicamento alterado com Sucesso!';
         header("Location:index.php");
