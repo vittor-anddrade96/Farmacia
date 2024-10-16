b54bccf2b53949dd4638e81942338ea140877354
vittor-anddrade96 <vittor.anddrade@gmail.com>
2024-10-15 22:42:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (isset($_POST['acao'])) {
         
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['user_email'] = $user['email'];
-        $_SESSION['user_role'] = $user['role'];
+        $_SESSION['user_cargo'] = $user['cargo'];
 
         
         if ($user['role'] === 'Administrador') {
