0e325c56926ec5be817cdf70b050e2dc56eb62b1
vittor-anddrade96 <vittor.anddrade@gmail.com>
2024-10-15 22:33:27 -0300

    Update home_admin.php

--- home_admin.php
+++ home_admin.php
@@ -18,7 +18,7 @@ $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
 <body>
     <header class="bg-light py-3">
         <div class="container d-flex justify-content-between align-items-center">
-            <a href="index.php"><img src="imagens/farmacia.png" alt="Logo Farmácia" width="150"></a>
+            <a href=""><img src="imagens/farmacia.png" alt="Logo Farmácia" width="150"></a>
             <div>
                 <a href="Login.php" class="btn btn-outline-danger btn-sm">Login</a>
             </div>
